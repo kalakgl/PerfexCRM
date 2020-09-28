b3db52b242ff7143e2a552716892b7d4e7e0339a
Ulrik Motzfeldt-Skovgaard <ulmo@qeqqata.gl>
2020-09-28 15:07:27 -0200

    Create danish_num_words_lang.php

---
+++ danish/danish_num_words_lang.php
@@ -0,0 +1,273 @@
+
+  1
+  2
+  3
+  4
+  5
+  6
+  7
+  8
+  9
+ 10
+ 11
+ 12
+ 13
+ 14
+ 15
+ 16
+ 17
+ 18
+ 19
+ 20
+ 21
+ 22
+ 23
+ 24
+ 25
+ 26
+ 27
+ 28
+ 29
+ 30
+ 31
+ 32
+ 33
+ 34
+ 35
+ 36
+ 37
+ 38
+ 39
+ 40
+ 41
+ 42
+ 43
+ 44
+ 45
+ 46
+ 47
+ 48
+ 49
+ 50
+ 51
+ 52
+ 53
+ 54
+ 55
+ 56
+ 57
+ 58
+ 59
+ 60
+ 61
+ 62
+ 63
+ 64
+ 65
+ 66
+ 67
+ 68
+ 69
+ 70
+ 71
+ 72
+ 73
+ 74
+ 75
+ 76
+ 77
+ 78
+ 79
+ 80
+ 81
+ 82
+ 83
+ 84
+ 85
+ 86
+ 87
+ 88
+ 89
+ 90
+ 91
+ 92
+ 93
+ 94
+ 95
+ 96
+ 97
+ 98
+ 99
+100
+101
+102
+103
+104
+105
+106
+107
+108
+109
+110
+111
+112
+113
+114
+115
+116
+117
+118
+119
+120
+121
+122
+123
+124
+125
+126
+127
+128
+129
+130
+131
+132
+133
+134
+135
+136
+<?php
+
+$lang['num_word_1'] = 'En';
+$lang['num_word_2'] = 'To';
+$lang['num_word_3'] = 'Tre';
+$lang['num_word_4'] = 'Fire';
+$lang['num_word_5'] = 'Fem';
+$lang['num_word_6'] = 'Seks';
+$lang['num_word_7'] = 'Syv';
+$lang['num_word_8'] = 'Otte';
+$lang['num_word_9'] = 'Ni';
+$lang['num_word_10'] = 'Ti';
+$lang['num_word_11'] = 'Elleve';
+$lang['num_word_12'] = 'Tolv';
+$lang['num_word_13'] = 'Tretten';
+$lang['num_word_14'] = 'Fjorten';
+$lang['num_word_15'] = 'Femten';
+$lang['num_word_16'] = 'Seksten';
+$lang['num_word_17'] = 'Sytten';
+$lang['num_word_18'] = 'Atten';
+$lang['num_word_19'] = 'Nitten';
+$lang['num_word_20'] = 'Tyve';
+$lang['num_word_21'] = 'Enogtyve';
+$lang['num_word_22'] = 'Toogtyve';
+$lang['num_word_23'] = 'Treogtyve';
+$lang['num_word_24'] = 'Fireogtyve';
+$lang['num_word_25'] = 'Femogtyve';
+$lang['num_word_26'] = 'Twenty Six';
+$lang['num_word_27'] = 'Syvogtyve';
+$lang['num_word_28'] = 'Twenty Eight';
+$lang['num_word_29'] = 'Tyve Ni';
+$lang['num_word_30'] = 'Tredive';
+$lang['num_word_31'] = 'Thirty One';
+$lang['num_word_32'] = 'Tredive to';
+$lang['num_word_33'] = 'Tredive tre';
+$lang['num_word_34'] = 'Fireogtredive';
+$lang['num_word_35'] = 'Femogtredive';
+$lang['num_word_36'] = 'Tredive seks';
+$lang['num_word_37'] = 'Tredive syv';
+$lang['num_word_38'] = 'Tredive otte';
+$lang['num_word_39'] = 'Tredive Ni';
+$lang['num_word_40'] = 'Fyrre';
+$lang['num_word_41'] = 'Forty One';
+$lang['num_word_42'] = 'Forty Two';
+$lang['num_word_43'] = 'Fyrre tre';
+$lang['num_word_44'] = 'Fireogfyrre';
+$lang['num_word_45'] = 'Femogfyrre';
+$lang['num_word_46'] = 'Forty Six';
+$lang['num_word_47'] = 'Fyrre syv';
+$lang['num_word_48'] = 'Forty Eight';
+$lang['num_word_49'] = 'Forty Nine';
+$lang['num_word_50'] = 'Femti';
+$lang['num_word_51'] = 'Fifty One';
+$lang['num_word_52'] = 'Femogtyve';
+$lang['num_word_53'] = 'Femti tre';
+$lang['num_word_54'] = 'Femogtyve';
+$lang['num_word_55'] = 'Femogtredive';
+$lang['num_word_56'] = 'Femogtyve seks';
+$lang['num_word_57'] = 'Femog syv';
+$lang['num_word_58'] = 'Fifty Eight';
+$lang['num_word_59'] = 'Femti Ni';
+$lang['num_word_60'] = 'Sixty';
+$lang['num_word_61'] = 'Sixty One';
+$lang['num_word_62'] = 'To og tres';
+$lang['num_word_63'] = 'Tre og tres';
+$lang['num_word_64'] = 'Fire og tres';
+$lang['num_word_65'] = 'Femogtres';
+$lang['num_word_66'] = 'Sixty Six';
+$lang['num_word_67'] = 'Sixty Seven';
+$lang['num_word_68'] = 'Sixty Eight';
+$lang['num_word_69'] = 'Sixty Ni';
+$lang['num_word_70'] = 'Seventy';
+$lang['num_word_71'] = 'Seventy One';
+$lang['num_word_72'] = 'Seventy Two';
+$lang['num_word_73'] = 'Seventy Three';
+$lang['num_word_74'] = 'Seventy Four';
+$lang['num_word_75'] = 'Femoghalvfjerds';
+$lang['num_word_76'] = 'Seventy Six';
+$lang['num_word_77'] = 'Seventy Seven';
+$lang['num_word_78'] = 'Seventy Eight';
+$lang['num_word_79'] = 'Seventy Nine';
+$lang['num_word_80'] = 'Åtti';
+$lang['num_word_81'] = 'Eighty One';
+$lang['num_word_82'] = 'Eighty Two';
+$lang['num_word_83'] = 'Åtti tre';
+$lang['num_word_84'] = 'Fire og firs';
+$lang['num_word_85'] = 'Fem og firs';
+$lang['num_word_86'] = 'Eighty Six';
+$lang['num_word_87'] = 'Eighty Seven';
+$lang['num_word_88'] = 'Eighty Eight';
+$lang['num_word_89'] = 'Eighty Nine';
+$lang['num_word_90'] = 'Halvfems';
+$lang['num_word_91'] = 'Ninety One';
+$lang['num_word_92'] = 'Niogtyve';
+$lang['num_word_93'] = 'Treogfems';
+$lang['num_word_94'] = 'Fire og halvfjerds';
+$lang['num_word_95'] = 'Femogfems';
+$lang['num_word_96'] = 'Seksogfems';
+$lang['num_word_97'] = 'Syv halvfems';
+$lang['num_word_98'] = 'Ninety Eight';
+$lang['num_word_99'] = 'Ninety Nine';
+$lang['num_word_100'] = 'Hundrede';
+$lang['num_word_200'] = 'To hundrede';
+$lang['num_word_300'] = 'Tre hundrede';
+$lang['num_word_400'] = 'Fire hundrede';
+$lang['num_word_500'] = 'Fem hundrede';
+$lang['num_word_600'] = 'Seks hundrede';
+$lang['num_word_700'] = 'Syv hundrede';
+$lang['num_word_800'] = 'Otte hundrede';
+$lang['num_word_900'] = 'Ni hundrede';
+$lang['num_word_thousand'] = 'Tusind';
+$lang['num_word_million'] = 'Million';
+$lang['num_word_billion'] = 'Milliard';
+$lang['num_word_trillion'] = 'billioner';
+$lang['num_word_zillion'] = 'Zillion';
+$lang['num_word_cents'] = 'Cents';
+$lang['number_word_and'] = 'Og';
+
+# For Indian Customers, used with INR currency
+$lang['num_word_hundred'] = 'Hundred';
+$lang['num_word_lakh'] = 'Lakh';
+$lang['num_word_lakhs'] = 'Lakh';
+
+// Show in invoices and estimates
+$lang['num_word'] = 'Med ord';
+
+$currencies = [
+    'USD' => 'Dollars',
+    'EUR' => 'Euros',
+];
+
+$currencies = hooks()->apply_filters('before_number_format_render_languge_currencies', $currencies);
+
+foreach ($currencies as $key => $val) {
+    $lang['num_word_' . strtoupper($key)] = $val;
+}
